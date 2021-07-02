14e8316b64028a956dbd6b0b9b3cc28b5426f50e
Gordon Hodanich <ghodanich@gmail.com>
2021-07-02 11:27:37 -0400

    done

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -2,16 +2,14 @@ import React, { useState } from "react"
 import UpdateComment from "./UpdateComment.js"
 
 const Comments = ({ comments }) => {
-
   return(
-  <div>
-    <h2>{comments.length} Comments</h2>
-  <ul>
-    {comments.map((comment) => (
-     <UpdateComment comment={comment}/>
-    ))}
-  </ul>
+    <div>
+      <h2>{comments.length} Comments</h2>
+    <ul>
+      {comments.map((comment) => (
+        <UpdateComment comment={comment}/>
+      ))}
+    </ul>
   </div>
   )}
-
 export default Comments
